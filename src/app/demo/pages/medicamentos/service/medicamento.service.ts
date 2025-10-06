ef6e286fbe13699fe47ca6bb442ea96372786aab
Nahum <nahum.dominguez@uniminuto.edu.co>
2025-10-05 23:20:49 -0500

    Se Implementa en su totalidad medicamentos con su funcionamiento

---
+++ src/app/demo/pages/medicamentos/service/medicamento.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from 'src/app/services/backend.service';
+import { Medicamento } from '../models/medicamento';
+import { environment } from './../../../../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MedicamentoService {
+
+    urlBase: string = environment.apiUrlAuth;
+    urlApi: string = 'medicamento';
+
+    constructor(private backendService: BackendService) { }
+
+    listarMedicamentos(): Observable<Medicamento[]> {
+    return this.backendService.get(this.urlBase, this.urlApi, 'listar');
+    }
+}
