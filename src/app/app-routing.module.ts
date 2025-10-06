0a534b886a4425c515d605b72e772f8e1adec722
Nahum <nahum.dominguez@uniminuto.edu.co>
2025-10-05 23:19:15 -0500

    Se configura ruta de cada Componente del menu de navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,9 @@ import { AdminComponent } from './theme/layout/admin/admin.component';
 import { UsuarioComponent } from './demo/pages/usuario/usuario.component';
 import { MedicoComponent } from './demo/pages/medico/medico.component';
 import { PacienteComponent } from './demo/pages/paciente/paciente.component';
-//import { EspecializacionComponent } from './demo/pages/paciente/especializacion.component';
-//import { HistoriaMedicaComponent } from './demo/pages/paciente/historiaMedicamneto.component';
-//import { MedicamentosComponent } from './demo/pages/paciente/medicamentos.component';
-//import { RecetasComponent } from './demo/pages/paciente/recetas.component';
+import { RecetaComponent } from './demo/pages/receta/receta.component';
 import { CitaComponent } from './demo/pages/citas/cita.component';
-
+import { MedicamentoComponent} from './demo/pages/medicamentos/medicamento.component';
 
 export const routes: Routes = [
   {
@@ -25,11 +22,9 @@ export const routes: Routes = [
         { path: 'usuario', component: UsuarioComponent, data: { title: 'Usuario' }} ,
         { path: 'medico', component: MedicoComponent, data: { title: 'Medico' }},
         { path: 'paciente', component: PacienteComponent, data: { title: 'Paciente' }},
-       //{ path: 'especilizaciones', component: EspecializacionComponent, data: { title: 'Especializaciones' }},
-       //{ path: 'historia_medica', component: HistoriaMedicaComponent, data: { title: 'Historia medica' }},
-       //{ path: 'medicamentos', component: MedicamentosComponent, data: { title: 'Medicamentos }},
-       //{ path: 'recetas', component: RecetasComponent, data: { title: 'Recetas' }},
+        { path: 'receta', component: RecetaComponent, data: { title: 'Receta' }},
         { path: 'citas', component: CitaComponent, data: { title: 'Citas' }},
+        { path: 'medicamento', component: MedicamentoComponent, data: { title: 'Medicamento' }}
     ]
   },
   { path: '**', redirectTo: 'inicio' }
