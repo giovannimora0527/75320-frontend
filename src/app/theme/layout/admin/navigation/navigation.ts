8c678562c491a2ec75b5df7e823d97971500d3c0
Whevil <diego.barrera-b@uniminuto.edu.co>
2025-10-11 13:51:12 -0500

    Actualizacion de menu lateral con modulos modluos solicitados

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -32,7 +32,7 @@ export const NavigationItems: NavigationItem[] = [
       /* ---------- Nuevos menus aqui -------------  */
       {
         id: 'medico',
-        title: 'Gestión de medicos',
+        title: 'GESTION MEDICOS',
         type: 'item',
         url: '/inicio/medico',
         icon: 'feather icon-users',
@@ -40,10 +40,57 @@ export const NavigationItems: NavigationItem[] = [
       },
       {
         id: 'paciente',
-        title: 'Gestión de pacientes',
+        title: 'GESTION PACIENTES',
         type: 'item',
         url: '/inicio/paciente',
         icon: 'feather icon-users',
+        classes: 'nav-item'
+      },
+      /*Buen dia, para el desarrollo de 
+      esta tarea es necesario crear los 
+      menus y los componentes faltantes.
+       Medicamentos, Citas, Formulas Medicas,
+        Historias Medicas, Gestión de especializaciones.*/
+
+      {
+        id: 'medicamento',
+        title: 'GESTION MEDICAMENTOS',
+        type: 'item',
+        url: '/inicio/paciente',
+        icon: 'feather icon-crosshair',
+        classes: 'nav-item'
+      },
+
+      {
+        id: 'cita',
+        title: 'GESTION CITAS',
+        type: 'item',
+        url: '/inicio/cita',
+        icon: 'feather icon-calendar',
+        classes: 'nav-item'
+      },
+      {
+        id: 'formulamedica',
+        title: 'GESTION FORMULA MEDICA',
+        type: 'item',
+        url: '/inicio/formulamedica',
+        icon: 'feather icon-file-text',
+        classes: 'nav-item'
+      },
+      {
+        id: 'historiamedica',
+        title: 'GESTION HISTORIA MEDICA',
+        type: 'item',
+        url: '/inicio/historiamedica',
+        icon: 'feather icon-folder',
+        classes: 'nav-item'
+      },
+      {
+        id: 'gestionespecializacion',
+        title: 'GESTION ESPECIALIZACION',
+        type: 'item',
+        url: '/inicio/gestionespecializacion',
+        icon: 'feather icon-book',
         classes: 'nav-item'
       },
     ]
