9211819b028c0952779f767ce5d81ef410e15557
Nahum <nahum.dominguez@uniminuto.edu.co>
2025-10-22 00:53:55 -0500

    Servicio q trae lo del back pa ver q pasa en el front ME MUERO

---
+++ src/app/demo/pages/formula/service/receta.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from 'src/app/services/backend.service';
+import { Recetas } from '../model/recetas';
+import { environment } from './../../../../../environments/environment';
+import { RecetaRq } from '../model/respuesta';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class RecetaService {   
+    urlBase: string = environment.apiUrlAuth;
+    urlApi: string = 'receta';
+
+    constructor(private backendService: BackendService) {}
+
+    listarRecetas(): Observable<Recetas[]> {
+    return this.backendService.get(this.urlBase, this.urlApi, 'listar');
+    } 
+    guardarReceta(Recetas: Recetas): Observable<RecetaRq> {   
+    return this.backendService.post(this.urlBase, this.urlApi, 'guardar', Recetas);
+    }
+
+    actualizarReceta(Recetas: Recetas): Observable<RecetaRq> {      
+    return this.backendService.post(this.urlBase, this.urlApi, 'actualizar', Recetas);
+    }
+}
