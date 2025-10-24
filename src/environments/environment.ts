af4ecdab74ee15147d1407f62c6732decbf74516
MaicolDa <tecnicolaboraldavid@gmail.com>
2025-10-24 00:04:59 -0500

    Agregado módulo de especializacines_parcial_se_corrige

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+import packageInfo from '../../package.json';
+
+export const environment = {
+  appVersion: packageInfo.version,
+  production: false,  
+  apiUrlAuth: 'http://localhost:8000/clinica/v1',
+  apiUrl: 'http://localhost:8000/clinica/v1'
+};
